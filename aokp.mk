dfbdd63e1c97e37ddc93281b91172e24440f62c1
Paletrox-Z <paletroxzpz@gmail.com>
2018-03-08 07:11:14 +0530

    Adding all sources back with prebuilt kernel

---
+++ aokp.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/xiaomi/oxygen/full_oxygen.mk)
+
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+
+PRODUCT_NAME := aokp_oxygen
+BOARD_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="Xiaomi/oxygen/oxygen:7.1.1/NMF26F/V9.0.3.0.NDDMIEI:user/release-keys" \
+    PRIVATE_BUILD_DESC="oxygen-user 7.1.1 NMF26F V9.0.3.0.NDDMIEI release-keys"
