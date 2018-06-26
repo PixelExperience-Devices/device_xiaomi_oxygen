a8911bac51573ffaf87570f983e8fa07eb80b3f5
v-v3k <paletroxzpz@gmail.com>
2018-06-26 03:25:45 +0000

    Update: Use Camera2 as default camera

--- device.mk
+++ device.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    DevCamera \
+    Camera2 \
     libshim_camera \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
