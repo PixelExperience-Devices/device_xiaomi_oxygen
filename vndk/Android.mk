d2955b0d2d9e38a3f4bc3defe0b59f5566b56110
Scissordragonboy (AyaanMalik) <37676941+Scissordragonboy@users.noreply.github.com>
2018-09-18 12:35:31 +0530

    Update Android.mk

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -32,7 +32,7 @@ EXTRA_VENDOR_LIBRARIES := \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
     vendor.display.color@1.0 \
-    vendor.display.config@1.0 \
+    vendor.display.config@1.0
     
 
 #-------------------------------------------------------------------------------
