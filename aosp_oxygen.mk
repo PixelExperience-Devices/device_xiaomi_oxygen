892fa306e03769b1bf22c448f2d11bad8acab4c7
halibw <bymark256@gmail.com>
2022-08-01 08:52:22 +0800

    oxygen: Build AOSP recovery
    Change-Id: I87df0bedf44e5c0df6dfcef8e7a97866cbebf551

--- aosp_oxygen.mk
+++ aosp_oxygen.mk
@@ -24,6 +24,7 @@ $(call inherit-product, device/xiaomi/oxygen/device.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
